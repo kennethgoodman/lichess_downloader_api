581bb8473632a3c12bfdabe4594a1fb7dcaa1a94
kennethgoodman <kennethsgoodman@yahoo.com>
2020-12-28 13:52:57 -0500

    switching to new version of header utils

--- filters/game_filters.py
+++ filters/game_filters.py
@@ -1,11 +1,18 @@
-def get_filter_by_result(result_wanted):
-	return lambda game: game.props.get_prop("Result") == result_wanted
+from python_chess_utils.header_utils import get_avg_rating, get_time_control, get_result
+
+
+def get_filter_by_result(result_wanted: str):
+	return lambda game: get_result(game) == result_wanted
+
 
 def get_filter_by_avg_rating(min_rating=0, max_rating=float('inf')):
-	get_avg_rating = lambda game: (game.get_white_elo() + game.get_black_elo())/2.0
 	return lambda game: min_rating <= get_avg_rating(game) <= max_rating
 
+
 def get_filter_by_time_control(min_time=0, min_increment=0, max_time=float('inf'), max_increment=float('inf')):
-	return lambda game: min_time <= game.get_time_control().base <= max_time and (
-		min_increment <= game.get_time_control().increment <= max_increment if game.get_time_control().increment is not None else True
+	return lambda game: get_time_control(game).in_between(
+		min_time=min_time,
+		min_increment=min_increment,
+		max_time=max_time,
+		max_increment=max_increment
 	)
